c709a93ba982e50b9eab57e474f18bacb05bac98
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2025-10-30 15:35:46 -0500

    fix: move code inside GET + keep my learning comments

--- src/app/api/summoner/route.js
+++ src/app/api/summoner/route.js
@@ -1,30 +1,32 @@
-// I am going to comment my thoughts of what i think everything is 
-export async function GET(request) { // i assume we are sending (exporting) a function an async (not sure what that means)function which will get a request(not sure from where)
-    const query = new URL(request.url).searchParams.get('name'); // then create a not changing variable called query which is new? then get a url of the request and look up the name maybe
-    if (!query || !query.includes('#')) { // if not query ???? (maybe the query does not exist) or the query does not include #
-        return new Response('Use: name#tag', { status: 400}); // return a new response that says to use name#tag not sure
-    }
-}
+export async function GET(request) {
+  // i assume we are sending (exporting) a function an async (not sure what that means)function which will get a request(not sure from where)
+  const url = new URL(request.url);
+  const query = url.searchParams.get('name'); // then create a not changing variable called query which is new? then get a url of the request and look up the name maybe
 
-const [gameName, tagLine] = query.split('#'); // then create a constant list which takes the query name#tag and splits it into the respective parts
-const key = process.env.RIOT_API_KEY; // then create constant variable which securely stores the api key
- 
-// get the puuid
+  if (!query || !query.includes('#')) { // if not query ???? (maybe the query does not exist) or the query does not include #
+    return new Response('Use: name#tag', { status: 400}); // return a new response that says to use name#tag not sure
+  }
 
-const accountUrl = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${key}`; // call the api using the key for the name tag
-const accountRes = await fetch(accountUrl); // no idea what this line does
-if (!accountRes) return new Response('Player not found', { status: 404 });  // no idea what this line does
-const { puuid } = await accountRes.json(); // no idea what this line does gets the puuid from something
+  const [gameName, tagLine] = query.split('#'); // then create a constant list which takes the query name#tag and splits it into the respective parts
+  const key = process.env.RIOT_API_KEY; // then create constant variable which securely stores the api key
 
-// get summoner data
-const summonerUrl = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${key}`; // using the link insert the key and the puuid we got 
-const summonerRes = await fetch(summonerUrl); // no idea
-const data = await summonerRes.json(); // no idea
+  // get the puuid
+  const accountUrl = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${key}`; // call the api using the key for the name tag
+  const accountRes = await fetch(accountUrl); // no idea what this line does
+  if (!accountRes.ok) return new Response('Player not found', { status: 404 });  // no idea what this line does
+  const { puuid } = await accountRes.json(); // no idea what this line does gets the puuid from something
 
-return new Response(JSON.stringify({
+  // get summoner data
+  const summonerUrl = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${key}`; // using the link insert the key and the puuid we got 
+  const summonerRes = await fetch(summonerUrl); // no idea
+  if (!summonerRes.ok) return new Response('Summoner error', { status: 500 });
+  const data = await summonerRes.json(); // no idea
+
+  return new Response(JSON.stringify({
     name: data.name,
     level: data.summonerLevel,
     icon: `https://ddragon.leagueoflegends.com/cdn/14.25.1/img/profileicon/${data.profileIconId}.png`
   }), {
     headers: { 'Content-Type': 'application/json' }
   });
+}
